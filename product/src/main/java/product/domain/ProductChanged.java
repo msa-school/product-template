c6671ec70d47616db38918913f2c378d78db77a5
박용주 <apexacme@gmail.com>
2022-10-16 19:31:10 +0900

    commit

---
+++ product/src/main/java/product/domain/ProductChanged.java
@@ -0,0 +1,22 @@
+package product.domain;
+
+import product.domain.*;
+import product.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class ProductChanged extends AbstractEvent {
+
+    private Long id;
+    private String name;
+    private Integer stock;
+
+    public ProductChanged(Product aggregate){
+        super(aggregate);
+    }
+    public ProductChanged(){
+        super();
+    }
+}
