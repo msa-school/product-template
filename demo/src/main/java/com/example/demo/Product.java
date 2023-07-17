93424178b7bbd8d89720ad33cdb82750654acd4f
박용주 <apexacme@gmail.com>
2023-07-17 19:40:18 +0900

    Update Product.java

--- demo/src/main/java/com/example/demo/Product.java
+++ demo/src/main/java/com/example/demo/Product.java
@@ -2,6 +2,10 @@ package com.example.demo;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Entity
 public class Product {
@@ -29,5 +33,21 @@ public class Product {
     public void setStock(int stock) {
         this.stock = stock;
     }
-   
+    
+   @PostPersist
+    public void eventPublish(){
+        ProductChanged productChanged = new ProductChanged();
+        productChanged.setProductId(this.getId());
+        productChanged.setProductName(this.getName());
+        productChanged.setProductStock(this.getStock());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = null;
+
+        try {
+            json = objectMapper.writeValueAsString(productChanged);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON format exception", e);
+        }
+    System.out.println(json);
+  }
 }
