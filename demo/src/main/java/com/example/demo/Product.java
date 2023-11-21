7c784ee77d271a12cc3eabebb40109152dc69a34
kibum0405 <top5w5@naver.com>
2023-11-21 01:10:55 +0000

    sds

--- demo/src/main/java/com/example/demo/Product.java
+++ demo/src/main/java/com/example/demo/Product.java
@@ -1,1 +1,33 @@
+package com.example.demo;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Product {
+
+    @Id @GeneratedValue
+    Long id;
+    String name;
+    int stock;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+}
